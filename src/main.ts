a9ae0f738fccea76b2004552c7b52f7ac1c23e02
Bohan <bohan.huang@coolbe.com.tw>
2023-10-11 10:58:53 +0800

    save

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,9 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
-  const corsOptions: CorsOptions = {
-    origin: '*', // 允许所有来源
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    preflightContinue: false,
-    optionsSuccessStatus: 204,
-  };
-  app.enableCors(corsOptions);
-
+  app.enableCors();
   await app.listen(3000);
 }
 bootstrap();
