614a924380eab71a4b5abc90f1cb4c35be0f8eeb
Bohan <bohan.huang@coolbe.com.tw>
2023-10-16 18:24:48 +0800

    save

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import helmet from 'helmet';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
-  app.use(helmet());
+  // app.use(helmet());
   await app.listen(3000);
 }
 bootstrap();
