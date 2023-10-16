8d54ca9eb69051000a27f99fe6280202061c0751
Bohan <bohan.huang@coolbe.com.tw>
2023-10-16 18:22:28 +0800

    save

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,13 @@ import { AppModule } from './app.module';
 import helmet from 'helmet';
 
 async function bootstrap() {
-  // const app = await NestFactory.create(AppModule);
-  const app = await NestFactory.create(AppModule, { cors: true });
-  // app.enableCors();
+  const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: 'https://10.10.50.56:8110',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    allowedHeaders: 'Content-Type, Accept',
+    credentials: true,
+  });
   app.use(helmet());
   await app.listen(3000);
 }
